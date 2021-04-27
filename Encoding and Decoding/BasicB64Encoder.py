ab00eb715ff3c3c4544d33513af41597faf54f21
azrael0learza <65418079+azrael0learza@users.noreply.github.com>
2021-04-28 01:22:46 +0200

    Basic Base64 Encoder
    Encoding base64 through basic pysimplegui

---
+++ Encoding and Decoding/BasicB64Encoder.py
@@ -0,0 +1,30 @@
+import base64
+import PySimpleGUI as sg
+
+
+layout = [[sg.Text("Enter Encoded Message")],
+          [sg.InputText()],
+          [sg.Text(size=(40,1), key='-OUTPUT-')],
+          [sg.Button('Ok'), sg.Button('Quit')]]
+ 
+
+window = sg.Window('Base64 Decoder', layout)
+
+
+while True:  
+    event, values = window.read()
+    if event in (None, 'Exit'):
+        break
+    
+    enco = values[0]
+    encoded = base64.b64encode(enco.encode())
+    
+    
+    if event == sg.WINDOW_CLOSED or event == 'Quit':
+        break
+    
+    
+    sg.popup(encoded)
+
+
+window.close()
