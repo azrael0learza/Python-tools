7f76ecbe6586be0aab2a136bd90a08fdb32c538f
azrael0learza <65418079+azrael0learza@users.noreply.github.com>
2021-05-13 23:01:22 +0200

    BasicTranslator_AutoToEnglish
    Basic English Translator  - Auto Detects any language and translates it to English using googletrans

---
+++ Language Translators/BasicGoogleTranslate_AutoDetectToEnglish.py
@@ -0,0 +1,40 @@
+from googletrans import Translator
+from pprint import pprint
+import PySimpleGUI as sg
+
+
+
+sg.theme('DarkBlue2')
+layout = [[sg.Text("Enter Text")],
+          [sg.Text('Translate this :') ,sg.InputText()],
+          [sg.Text(size=(40,1), key='-OUTPUT-')],
+          [sg.Button('Translate'), sg.Button('Quit')]]
+
+window = sg.Window('Basic Py Translator', layout)
+
+
+while True:  
+        event, values = window.read()
+        if event in (None, 'Exit'):
+            break
+    
+        iniTran = values[0]
+        translator = Translator()
+
+        finTran = translator.translate(iniTran)
+
+        if event == sg.WINDOW_CLOSED or event == 'Quit':
+            break
+    
+        sg.Print(do_not_reroute_stdout=False)
+        pprint('Original text was: ' + values[0]) 
+        pprint('Detected language: ' + finTran.src)
+    
+        pprint('-----------------------------------------------------------------------------')
+        
+        pprint('Translated language: ' + finTran.dest)
+        pprint(finTran.text)
+        
+        
+window.close()
+
